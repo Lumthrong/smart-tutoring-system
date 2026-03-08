98be60b21594fd482b0ecb3c26a98f27312fcad6
Lumthrong <iamrein22@gmail.com>
2026-03-08 11:08:51 +0530

    Change email transport configuration to use SMTP

--- server.js
+++ server.js
@@ -56,7 +56,9 @@ const upload = multer({
 const otpStore = new Map();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS

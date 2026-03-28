5936c7634431acf11455862c69ffb292d80e223c
Lumthrong <iamrein22@gmail.com>
2026-03-29 04:09:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -995,7 +995,7 @@ ${text}
 async function waitForWhisperReady() {
   for (let i = 0; i < 25; i++) {
     try {
-      const res = await fetch("https://whisper-api-nkv2.onrender.com/health");
+      const res = await fetch("https://marcell-uncaused-nonproblematically.ngrok-free.dev/health");
 
       console.log("STATUS:", res.status);
 
@@ -1098,7 +1098,7 @@ app.post("/generate-transcript", async (req, res) => {
           for (let i = 0; i < 5; i++) {
             try {
               return await axios.post(
-                "https://whisper-api-nkv2.onrender.com/transcribe",
+                "https://marcell-uncaused-nonproblematically.ngrok-free.dev/transcribe",
                 formData,
                 {
                   headers: {
@@ -1164,7 +1164,7 @@ app.get("/transcript-status/:jobId", async (req, res) => {
   try {
 
     const response = await fetch(
-      `https://whisper-api-nkv2.onrender.com/status/${jobId}`
+      `https://marcell-uncaused-nonproblematically.ngrok-free.dev/status/${jobId}`
     );
 
     const text = await response.text();
